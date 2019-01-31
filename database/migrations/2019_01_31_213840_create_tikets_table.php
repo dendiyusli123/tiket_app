d36cf021912672f186bda16828fbbfc17d4df498
hackass <hackass@localhost.localdomain>
2019-01-31 13:54:00 -0800

    created pagescontroller and maked first migration

---
+++ database/migrations/2019_01_31_213840_create_tikets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTiketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tikets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('judul', 255);
+            $table->text('konten');
+            $table->string('tautan')->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tikets');
+    }
+}
